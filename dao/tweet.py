955bfe10a95feec9b7736f7c4e2223a4868a321c
Xinyue211 <xzhang4@andrew.cmu.edu>
2020-03-30 13:27:10 -0400

    Remove User Schema

--- dao/tweet.py
+++ dao/tweet.py
@@ -1,15 +1,6 @@
-import user
-
 
 def add_tweet(db, tweet_json):
-    user_list = db.user.find({'id': tweet_json['user']['id']})
-    if len(user_list) > 0:
-        tweet_user = user_list[0]
-    else:
-        user.add_user(db, tweet_json['user'])
-        tweet_user = db.user.find({'id': tweet_json['user']['id']})[0]
-    tweet_json['user'] = tweet_user['_id']
-
+    # Add quote reference if there is nonempty 'quoted_status' in the tweet's json
     if tweet_json['quoted_status']:
         q_list = db.tweet.find({'id': tweet_json['quoted_status']['id']})
         if len(q_list) > 0:
@@ -19,6 +10,7 @@ def add_tweet(db, tweet_json):
             quote = db.tweet.find({'id': tweet_json['quoted_status']['id']})[0]
         tweet_json['quoted_status'] = quote['_id']
 
+    # Add retweet reference if there is nonempty 'retweeted_status' in the tweet's json
     if tweet_json['retweeted_status']:
         t_list = db.tweet.find({'id': tweet_json['retweeted_status']['id']})
         if len(t_list) > 0:
